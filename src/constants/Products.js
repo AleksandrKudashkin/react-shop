c989db08f5376f265116ba42b32e62fd41da863c
Alex Kudashkin <alex@kudashkin.pro>
2018-08-11 11:43:19 -0400

    Add a list of Products

---
+++ src/constants/Products.js
@@ -0,0 +1,20 @@
+export const Products = [
+  {
+    id: 1,
+    title: 'Assam tea',
+    price: 1050,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/bf0/bf0a7397462e387d126a692d4f3c161c.jpg'
+  },
+  {
+    id: 2,
+    title: 'Milk Oolong',
+    price: 1790,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/b3e/b3e0bff7b71643b050c2abcbcc8173ff.jpg'
+  },
+  {
+    id: 1,
+    title: 'Earl Grey',
+    price: 565,
+    imageUrl: 'https://www.rusteaco.ru/upload/iblock/3be/3beaf7a17f16f453d4a21ec635c04830.jpg'
+  }
+];
