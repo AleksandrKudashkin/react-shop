26c87865b326d088af9e90e23c72f0645bfc8023
Alex Kudashkin <alex@kudashkin.pro>
2018-08-12 08:23:32 -0400

    Add TextBox

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Image from './Image';
 import Products from '../constants/Products';
+import TextBox from './TextBox';
 
 const MainPage = () => (
-  <Image src={Products[0].imageUrl} width="570" height="427" alt="The tea" />
+  <div>
+    <p>
+      <TextBox text="Sample tea" />
+    </p>
+    <Image src={Products[0].imageUrl} width="570" height="427" alt="The tea" />
+  </div>
 );
 
 export default MainPage;
