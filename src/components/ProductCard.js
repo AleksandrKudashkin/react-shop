c047656b688cbba35a4d23955a0663db32912e78
Alex Kudashkin <alex@kudashkin.pro>
2018-08-22 20:34:27 -0400

    Extract AddToCart to another component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,50 +2,19 @@ import React from "react";
 import TextBox from "./TextBox";
 import Image from "./Image";
 import Price from "./Price";
-import CartContext from "../modules/CartContext";
+import AddToCart from "./AddToCart";
 
-
-class ProductCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      amount: this.props.amount
-    }
-  }
-
-  setField(e, name) {
-    this.setState(Object.assign({}, this.state, {[name]: e.target.value}))
-  }
-
-  render() {
-    const {product} = this.props;
-    const {title, price, imageUrl} = this.props.product;
-    return <div>
-      <TextBox text={title} />
-      <br />
-      <Price price={price} />
-      <br />
-      <Image src={imageUrl} />
-      <br />
-      <CartContext.Consumer>
-        {({addToCart}) =>
-          <button onClick={() => addToCart(product, this.state.amount)}>
-            Add to the cart
-          </button>
-        }
-      </CartContext.Consumer>
-      <label htmlFor='amount'>Amount:</label>
-      <input name='amount'
-             id={`${product.id}-amount`}
-             value={this.state.amount}
-             onChange={(e) => this.setField(e, 'amount')}
-      />
-    </div>
-  }
-}
-
-ProductCard.defaultProps = {
-  amount: 1
+const ProductCard = ({ product }) => {
+  const {title, price, imageUrl} = product;
+  return <div>
+    <TextBox text={title} />
+    <br />
+    <Price price={price} />
+    <br />
+    <Image src={imageUrl} />
+    <br />
+    <AddToCart product={product}/>
+  </div>
 };
 
 export default ProductCard;
